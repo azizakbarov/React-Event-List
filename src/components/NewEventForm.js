11e6001ed18c08d1d8a6c4c50aaacc1c7551c66b
Aziz Akbarov <azizbakbarov@gmail.com>
2022-01-07 14:47:17 +0500

    select boxes

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,40 +1,56 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import "./NewEventForm.css";
 
 export default function NewEventForm({ addEvent }) {
-  // const [title, setTitle] = useState("");
-  // const [date, setDate] = useState("");
-
-  const title = useRef();
-  const date = useRef();
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [location, setLocation] = useState("manchester");
 
   const resetForm = () => {
-    title.current.value = "";
-    date.current.value = "";
+    setTitle("");
+    setDate("");
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    console.log(title, date);
-
     const event = {
-      title: title.current.value,
-      date: date.current.value,
+      title: title,
+      date: date,
+      location: location,
       id: Math.floor(Math.random() * 10000),
     };
-
+    console.log(event);
     addEvent(event);
     resetForm();
   };
+
   return (
     <form className="new-event-form" onSubmit={handleSubmit}>
       <label>
         <span>Event Title:</span>
-        <input type="text" ref={title} />
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          required
+        />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" ref={date} />
+        <input
+          type="date"
+          onChange={(e) => setDate(e.target.value)}
+          value={date}
+          required
+        />
+      </label>
+      <label>
+        <span>Event Location:</span>
+        <select onChange={(e) => setLocation(e.target.value)}>
+          <option value="manchester">Manchester</option>
+          <option value="london">London</option>
+          <option value="cardiff">Cardiff</option>
+        </select>
       </label>
       <button>Submit</button>
     </form>
